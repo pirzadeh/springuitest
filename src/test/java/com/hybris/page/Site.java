32cf0686f39b1b36d33c93f1cb515745e9a0e013
anurag.thakur <anurag.thakur@hybris.com>
2014-01-28 15:13:33 -0500

    Modified Site.java

--- src/test/java/com/hybris/page/Site.java
+++ src/test/java/com/hybris/page/Site.java
@@ -1,6 +1,7 @@
 package com.hybris.page;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.hybris.page.base.Page;
 import com.hybris.page.base.PageObject;
@@ -8,13 +9,15 @@ import com.hybris.page.base.PageObject;
 @Page
 public class Site extends PageObject
 {
+	@Value("${BASE_URL}")
+	private String baseURL;
 	
 	@Autowired
 	private PowerHome powerHome; 
 	
 	public PowerHome gotoPowerHome()
 	{
-		getDriver().get("http://powertools.dev-com4-2.yrdrt.fra.hybris.com:9001/yb2bacceleratorstorefront/");
+		getDriver().get(baseURL);
 		getDriver().manage().window().maximize();
 		return powerHome;
 	 }
