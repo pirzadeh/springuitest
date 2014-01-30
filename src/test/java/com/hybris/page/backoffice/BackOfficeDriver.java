193b0cf6a1536b2cd891b6d55507a6845ca22cae
Amir <heidar.pirzadeh@hybris.com>
2014-01-30 11:41:34 -0500

    new test for OMSUI

--- src/test/java/com/hybris/page/backoffice/BackOfficeDriver.java
+++ src/test/java/com/hybris/page/backoffice/BackOfficeDriver.java
@@ -7,14 +7,14 @@ import com.hybris.page.base.Page;
 import com.hybris.page.base.PageObject;
 
 @Page
-public class BackOfficeSite extends PageObject
+public class BackOfficeDriver extends PageObject
 {
 	private String baseURL = "http://dev-xc2-2.yrdrt.fra.hybris.com:9001/backoffice/login.zul";
 	
 	@Autowired
 	private BackOfficeLogin backOfficeLogin; 
 	
-	public BackOfficeLogin gotoPowerHome()
+	public BackOfficeLogin gotoBackoffice()
 	{
 		getDriver().get(baseURL);
 		getDriver().manage().window().maximize();
---
+++ src/test/java/com/hybris/test/omsui/OMSUITest.java
@@ -0,0 +1,43 @@
+package com.hybris.test.omsui;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import com.hybris.page.backoffice.BackOfficeDriver;
+import com.hybris.page.backoffice.BackOfficeHome;
+import com.hybris.page.backoffice.BackOfficeLogin;
+import com.hybris.test.base.SeleniumTestCase;
+
+
+public class OMSUITest extends SeleniumTestCase {
+
+
+	@Autowired
+	private BackOfficeDriver backOfficeSite;
+
+	@Autowired
+	private WebDriverWait letsWait;
+
+	@Test
+	public void testHomePageSearch() {
+
+		// Given I am on the Hybris home page
+		BackOfficeLogin backOfficeLogin = backOfficeSite.gotoBackoffice();
+
+		// When I enter "drill" on the page and click on search button
+		/*
+		 * PowerSearchResultsPage searchResultsPage =
+		 * homePage.searchFor(searchTerm);
+		 */
+		BackOfficeHome backOfficeHome = backOfficeLogin.login("admin", "nimda");
+		// Then I should navigate to search results page
+
+		assertThat(backOfficeHome.getTitle()).contains("hybris Backoffice");
+
+	}
+
+}
